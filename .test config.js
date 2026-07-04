5a128605a31d0516f01e70246f9f130df4fe2a50
kare612 <yousseftrefoil@gmail.com>
2026-07-04 16:31:28 +0100

    Update bot configuration in .test config.js
    Removed the execute function and added bot configuration details.

---
+++ .test config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    botName: "كاكاشي بوت ⚡",
+    developerName: "Kare612",
+    developerNumber: "212784776925@s.whatsapp.net",
+    prefix: ".",
+    guildSystem: {
+        maxAdmins: 10,
+        unlimitedAccessForDev: true
+    },
+    apis: {
+        videoDownloader: "https://screenshotlayer.com",
+        decorationApi: "internal"
+    }
+};
